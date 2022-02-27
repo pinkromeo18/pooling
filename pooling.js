9e563ea5a29af9f0ff52ce526d2b65eef3a92bca
ピンクロメオ <99705203+pinkromeo18@users.noreply.github.com>
2022-02-27 10:47:59 +0900

    Create pooling.js

---
+++ pooling.js
@@ -0,0 +1,26 @@
+;(function(root){
+  'use-strict'
+  const sleep = s => new Promise(r => setTimeout(r, s));
+  const pooling = async (caller,limit) =>{
+    limit = limit || 60*1000 //def one min
+    var count =~~(limit/60) + 1
+    for(var i=0;i<count;i++){
+      if(caller()) return true;
+      await sleep(60)
+    }  
+    console.error('timeout pooling')
+    return false; 
+  }
+  root.sleep = sleep;
+  root.pooling =pooling;
+}(window||this));
+
+/* test code
+var a;
+setTimeout(()=>{ a='aiuewo'},3*1000)
+
+;(async ()=>{  
+  await pooling(()=>a)
+  document.body.textContent = a;  
+})();
+*/
